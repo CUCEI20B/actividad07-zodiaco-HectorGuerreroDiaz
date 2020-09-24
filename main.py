aa34df23a1a154ab0ef7cc7cc9d7d600a7482930
Hector Guerrero Diaz <hjesus.guerrero@alumnos.udg.mx>
2020-09-24 08:23:54 -0500

    prueba001

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hola Mundo")
+from numpy import array
+
+FechaInicio = array([22, 21, 20, 21, 21, 21, 22, 23, 24, 23, 23, 23])
+SignosZodiacales = array(["Capricornio","Acuario","Piscis","Aries","Tauro","Geminis","Cancer","Leo","Virgo","Libra","Escorpio","Sagitario"])
+
+def Zodiaco(day, month):
+    temp = month - 1
+    if(FechaInicio[month] < day):
+        temp = temp + 1
+    return SignosZodiacales[temp]
+
+
+day = int(input("Digame su dia: "))
+month = int(input("Digame su mes: "))
+result = Zodiaco(day, month)
+print(result)
